d59745224ab530afc4c362cba1bafa55d152711d
Dante Ricketts <dante.ricketts16@gmail.com>
2023-09-16 22:47:49 -0400

    Step 9 done

---
+++ lab-tunley/AlbumCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Dante Ricketts on 9/16/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+}
--- lab-tunley/AlbumsViewController.swift
+++ lab-tunley/AlbumsViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import Nuke
 
-class AlbumsViewController: UIViewController {
+class AlbumsViewController: UIViewController, UICollectionViewDataSource {
+    
+    
     var albums: [Album] = []
+    @IBOutlet weak var collectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
         
         // Create a search URL for fetching albums (`entity=album`)
         let url = URL(string: "https://itunes.apple.com/search?term=yeat&attribute=artistTerm&entity=album&media=music")!
@@ -33,7 +39,11 @@ class AlbumsViewController: UIViewController {
             do {
                 let response = try decoder.decode(AlbumSearchResponse.self, from: data)
                 let albums = response.results
-                self?.albums = albums
+                DispatchQueue.main.async {
+                    self?.albums = albums
+                    self?.collectionView.reloadData()
+                }
+                
                 
             } catch {
                 print("❌ Error parsing JSON: \(error.localizedDescription)")
@@ -43,7 +53,17 @@ class AlbumsViewController: UIViewController {
         // Initiate the network request
         task.resume()
     }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        albums.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCell
+        let album = albums[indexPath.item]
+        let imageUrl = album.artworkUrl100
+        Nuke.loadImage(with: imageUrl, into: cell.albumImageView)
+        return cell
+    }
 
     /*
     // MARK: - Navigation
